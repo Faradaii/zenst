e485a5f8664e4dfb3674c810a06c7e9be056c793
Faradaii <kdkfrdy@gmail.com>
2024-06-30 22:58:43 +0800

    Chore: remove unnecessary field

--- lib/Models/apis/videos.dart
+++ lib/Models/apis/videos.dart
@@ -4,11 +4,7 @@ class Video {
   final String title;
   final String cover;
   final String videoUrl;
-  final String musicUrl;
   final String musicCover;
-  final int duration;
-  final int createTime;
-  final String region;
   final Author author;
   final int diggCount;
   final int commentCount;
@@ -18,11 +14,7 @@ class Video {
     required this.title,
     required this.cover,
     required this.videoUrl,
-    required this.musicUrl,
     required this.musicCover,
-    required this.duration,
-    required this.createTime,
-    required this.region,
     required this.author,
     required this.diggCount,
     required this.commentCount,
@@ -34,11 +26,7 @@ class Video {
       title: json['title'],
       cover: json['cover'],
       videoUrl: json['play'],
-      musicUrl: json['music_info']['play'],
       musicCover: json['music_info']['cover'],
-      duration: json['duration'],
-      createTime: json['create_time'],
-      region: json['region'],
       author: Author.fromJson(json['author']),
       diggCount: json['digg_count'],
       commentCount: json['comment_count'],
@@ -47,13 +35,11 @@ class Video {
 }
 
 class Author {
-  final String id;
   final String uniqueId;
   final String nickname;
   final String avatar;
 
   Author({
-    required this.id,
     required this.uniqueId,
     required this.nickname,
     required this.avatar,
@@ -61,7 +47,6 @@ class Author {
 
   factory Author.fromJson(Map<String, dynamic> json) {
     return Author(
-      id: json['id'],
       uniqueId: json['unique_id'],
       nickname: json['nickname'],
       avatar: json['avatar'],
